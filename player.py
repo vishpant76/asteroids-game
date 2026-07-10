e4f62e14312e965bbf202eaedc9c72edebf616e5
Vishal Pant <vishal.pant1@gmail.com>
2026-07-10 16:48:21 +0530

    CH3: L3 - Moving Around - draw, update and flip to rotate the spaceship

---
+++ player.py
@@ -0,0 +1,32 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+
+    def triangle(self) -> list[pygame.Vector2]:
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen: pygame.Surface):
+        pygame.draw.polygon(screen, "white", self.triangle(), LINE_WIDTH)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt: float) -> None:
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
