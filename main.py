bbc0b7b65102cec085416ece6b4caddf7389af8c
Vishal Pant <vishal.pant1@gmail.com>
2026-07-10 19:32:59 +0530

    CH4: L2 - add collision detection logic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sys
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -41,6 +42,13 @@ def main():
         # player.update(dt)
 
         updatable.update(dt)
+
+        for obj in asteroids:
+            if obj.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+
         for obj in drawable:
             obj.draw(screen)
 
