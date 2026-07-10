73a6bc8142d276892cff2cf80d870eb66a7f3323
Vishal Pant <vishal.pant1@gmail.com>
2026-07-10 16:46:54 +0530

    CH3: L3 - Moving Around - draw, update and flip to rotate the spaceship

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 
 def main():
@@ -10,6 +11,7 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
 
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         log_state()
         for event in pygame.event.get():
@@ -17,6 +19,10 @@ def main():
                 return
 
         screen.fill("black")
+
+        player.draw(screen)
+
+        player.update(dt)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
